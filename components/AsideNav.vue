ff13404c94f6fa43fdefeacf6c5eec52f3284ac4
sallykwak <kwak0630@gmail.com>
2024-11-27 16:19:11 +0900

    Nuxt JS

---
+++ components/AsideNav.vue
@@ -0,0 +1,69 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="aside-nav">
+    <div class="nav-title">
+      <img :src="require('../assets/images/sally.png')" class="logo">
+      <p>NuxtJS Guide</p>
+    </div>
+    <ul class="nav-list">
+      <!-- <li><NuxtLink to="/main">Index</NuxtLink></li>
+      <li><NuxtLink to="/color">Color</NuxtLink></li> -->
+      <li
+        v-for="nav in navigations"  
+        :key="nav.name">
+        <NuxtLink
+          :to="nav.href"
+          active-class="active"
+          class="nav-link">
+          {{ nav.name }}
+        </NuxtLink> 
+      </li>
+    </ul>
+  </div>
+</template>
+  
+<script>
+export default {
+  name: 'AsideNav',
+  
+  data() {
+    return {
+      navigations: [
+        {
+          name: 'Index',
+          href: '/guide/'
+        },
+        {
+          name: 'Color',
+          href: '/guide/color'
+        },
+        // {
+        //   name: 'Text',
+        //   href: '/guide/text'
+        // },
+        {
+          name: 'Form',
+          href: '/guide/form'
+        },
+        {
+          name: 'Component',
+          href: '/guide/component'
+        },
+        {
+          name: 'Swiper',
+          href: '/guide/swiper'
+        },
+        {
+          name: 'Popup',
+          href: '/guide/popup'
+        },
+        {
+          name: 'Video',
+          href: '/guide/video'
+        },
+      ]
+    }
+  }
+}
+</script>
+  
---
+++ pages/guide/text.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="guide-wrap">
+    <AsideNav/>
+    <div class="guide-content">
+      <h2 class="guide-title">Text</h2>
+      <div class="guide-box text">
+        <p>세상에 이런 폰트가 나오다니 Regluar</p>
+        <br/>
+        <p style="font-weight:700;">세상에 이런 폰트가 나오다니 Bold</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default ({
+})
+</script>
